55d50d438fe88d99ce594011277983986945f5fc
Rune Niska <niska.rune@gmail.com>
2021-08-31 14:54:05 +0300

    Add info page

--- index.js
+++ index.js
@@ -31,6 +31,14 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    const date = new Date()
+    res.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>`
+    )
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
