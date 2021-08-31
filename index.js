7c36b88cc995628ff2a63930abfa0b3069947e23
Rune Niska <niska.rune@gmail.com>
2021-08-31 15:10:11 +0300

    Support DELETE call for each contact API endpoint

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ app.get('/api/persons/:id', (req, res) => {
         res.status(404).end()
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    res.status(204).end()
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
