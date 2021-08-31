3208bb76de2c8a1a32e08d4671d4c707d9cd7a3d
Rune Niska <niska.rune@gmail.com>
2021-08-31 14:41:15 +0300

    Create REST API for all contacts

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+const PORT = 3001
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Phonebook server')
+app.use(express.json())
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
