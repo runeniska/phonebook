663fa59c588a53c614074f0f2f0a963818a96442
Rune Niska <niska.rune@gmail.com>
2021-09-08 13:00:31 +0300

    Update the application to use MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const PORT = process.env.PORT || 3001
-
+require('dotenv').config()
+const PORT = process.env.PORT
+const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
@@ -14,58 +15,22 @@ app.use(express.static('build'))
 app.use(logger)
 app.use(cors())
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
-
-// Functions
-const generateId = () => {
-    const id = Math.floor(Math.random() * 1000000)
-    return id
-}
-
 
 // GET
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(personList => res.json(personList))
 })
 
 app.get('/info', (req, res) => {
     const date = new Date()
-    res.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
+    Person.find({}).then(personList => res.send(
+        `<p>Phonebook has info for ${personList.length} people</p>
         <p>${date}</p>`
-    )
+    ))
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-
-    if (person)
-        res.json(person)
-    else
-        res.status(404).end()
+    Person.findById(req.params.id).then(person => res.json(person))
 })
 
 
@@ -75,31 +40,18 @@ app.post('/api/persons', (req, res) => {
     const name = body.name
     const number = body.number
 
-    if (!name)
-        return res.status(400).json({ error: 'name missing' })
-    else if (!number)
-        return res.status(400).json({ error: 'number missing'})
-    else if (persons.find(p => p.name === name))
-        return res.status(400).json({ error: 'name already exists in the phonebook'})
-
-    const person = {
+    const person = new Person({
         name: name,
         number: number,
-        id: generateId()
-    }
+    })
 
-    persons = persons.concat(person)
-
-    res.json(person)
+    person.save().then(savedPerson => res.json(savedPerson))
 })
 
 
 // DELETE
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    res.status(204).end()
+    Person.findByIdAndRemove(req.params.id).then(result => res.status(204).end())
 })
 
 
